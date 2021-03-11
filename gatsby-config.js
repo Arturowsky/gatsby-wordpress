dcdd3e37b56beb7caeec00487c2f3b1311c7e65b
Artur <61249029+Arturowsky@users.noreply.github.com>
2021-03-12 00:54:38 +0100

    ui
    ggy

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'https://wordpress.com/page/eminem394020062.wordpress.com/5',
+        baseUrl: 'https://wordpress.com/page/eminem394020062.wordpress.com/',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: true,
         // The protocol. This can be http or https.
